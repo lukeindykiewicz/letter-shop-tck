cf66922562ec8642f3ba9c923f9ba66b8e20428a
Łukasz Indykiewicz <lukasz.indykiewicz@gmail.com>
2016-06-10 00:43:51 +0200

    Include first putting letters before getting it back

--- src/test/scala/lettershop/GetLettersFromCart.scala
+++ src/test/scala/lettershop/GetLettersFromCart.scala
@@ -3,22 +3,29 @@ package lettershop
 import org.specs2.mutable.Specification
 import org.specs2.concurrent.ExecutionEnv
 
+import scala.concurrent.Future
+
 import akka.http.scaladsl.model._
 import upickle.default._
 
 case class Cart(letters: String)
 
-class GetLettersIntoCart(implicit ee: ExecutionEnv)
+class GetLettersFromCart(implicit ee: ExecutionEnv)
     extends Specification
     with BaseTckTest {
 
   "GetLettersFromCart" should {
 
-    val getCart = go(HttpRequest(uri = url("cart")))
+    val cart123 = "cart/123"
+    val fooBAR = "fooBAR"
+    val  getCart: Future[HttpResponse] = for {
+      _ <- go(HttpRequest(method = HttpMethods.PUT, uri = url(s"$cart123/$fooBAR")))
+      get <- go(HttpRequest(uri = url(cart123)))
+    } yield get
 
     "get letters from cart" >> {
       getCart.map(_.status) should === (ok200).awaitFor(timeout)
-      body(getCart).map(read[Cart]) should === (Cart("abc")).awaitFor(timeout)
+      body(getCart).map(read[Cart]) should === (Cart(fooBAR)).awaitFor(timeout)
     }
 
   }
