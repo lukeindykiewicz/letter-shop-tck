5c5c9e794adae3ebb05c708b7cc7cc04dcce020e
Łukasz Indykiewicz <lukasz.indykiewicz@gmail.com>
2016-06-09 13:24:52 +0200

    Extract ops for request and assertions on response

--- src/test/scala/lettershop/BaseTckTest.scala
+++ src/test/scala/lettershop/BaseTckTest.scala
@@ -1,12 +1,16 @@
 package lettershop
 
 import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.HttpRequest
+import akka.http.scaladsl.model.HttpResponse
 import akka.http.scaladsl.model.StatusCode
 import akka.http.scaladsl.model.StatusCodes
 import akka.stream.ActorMaterializer
 import com.typesafe.config.ConfigFactory
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
+import scala.concurrent.Future
 
 import scala.language.postfixOps
 
@@ -21,10 +25,17 @@ trait BaseTckTest{
 
   private lazy val host = conf.getString("host")
   private lazy val port = conf.getString("port")
-  lazy val url = s"http://$host:$port"
 
   val ok200: StatusCode = StatusCodes.OK
 
   val timeout = 2 seconds
+
+  def go(request: HttpRequest): Future[HttpResponse] =
+    Http().singleRequest(request)
+
+  def url(path: String): String = s"http://$host:$port/$path"
+
+  def body(respF : Future[HttpResponse]): Future[String] = 
+    respF.flatMap(_.entity.toStrict(timeout).map(_.data.utf8String))
 
 }
--- src/test/scala/lettershop/GetLettersFromCart.scala
+++ src/test/scala/lettershop/GetLettersFromCart.scala
@@ -3,9 +3,7 @@ package lettershop
 import org.specs2.mutable.Specification
 import org.specs2.concurrent.ExecutionEnv
 
-import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
-import scala.concurrent.Future
 import upickle.default._
 
 case class Cart(letters: String)
@@ -14,13 +12,13 @@ class GetLettersIntoCart(implicit ee: ExecutionEnv)
     extends Specification
     with BaseTckTest {
 
-  val cartF: Future[HttpResponse] = Http().singleRequest(HttpRequest(method = HttpMethods.GET, uri = s"$url/cart"))
-
   "GetLettersFromCart" should {
 
+    val getCart = go(HttpRequest(uri = url("cart")))
+
     "get letters from cart" >> {
-      cartF.map(_.status) should === (ok200).awaitFor(timeout)
-      cartF.flatMap(_.entity.toStrict(timeout).map(_.data.utf8String)).map(data => read[Cart](data)) should === (Cart("abc")).awaitFor(timeout)
+      getCart.map(_.status) should === (ok200).awaitFor(timeout)
+      body(getCart).map(read[Cart]) should === (Cart("abc")).awaitFor(timeout)
     }
 
   }
