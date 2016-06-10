3209a9f1c81874c3dd1de26709c62f4809969ad8
Łukasz Indykiewicz <lukasz.indykiewicz@gmail.com>
2016-06-10 16:14:43 +0200

    Add test for default letter price

--- src/test/scala/lettershop/PutLetterPriceTest.scala
+++ src/test/scala/lettershop/PutLetterPriceTest.scala
@@ -19,18 +19,21 @@ class PutLetterPriceTest(implicit ee: ExecutionEnv)
 
     val letter = "A"
     val price: Double = 13.5
-    val cartId = "888"
-    val cart = s"cart/cartId"
 
     val priceLetter = go(HttpRequest(
       method = PUT,
       uri = url(s"price/$letter"),
       entity = HttpEntity(ContentTypes.`application/json`, write(Price(price)))))
 
-    val checkPrice = for {
+    def checkPrice(cartId: String, letter: String) =
+      for {
+        _ <- go(HttpRequest(method = PUT, uri = url(s"cart/$cartId/$letter")))
+        check <- go(HttpRequest(uri = url(s"check/$cartId")))
+      } yield check
+
+    val all = for {
       _ <- priceLetter
-      _ <- go(HttpRequest(method = PUT, uri = url(s"$cart/$letter")))
-      check <- go(HttpRequest(uri = url(s"check/$cartId")))
+      check <- checkPrice("888", letter)
     } yield check
 
     "respond with status 200" >> {
@@ -38,7 +41,11 @@ class PutLetterPriceTest(implicit ee: ExecutionEnv)
     }
 
     "letter should have set cost" >> {
-      body(checkPrice).map(read[Price]) should === (Price(price)).awaitFor(timeout) 
+      body(all).map(read[Price]) should === (Price(price)).awaitFor(timeout) 
+    }
+
+    "default letter price is 10" >> {
+      body(checkPrice("777", "x")).map(read[Price]) should === (Price(10)).awaitFor(timeout)
     }
 
   }
