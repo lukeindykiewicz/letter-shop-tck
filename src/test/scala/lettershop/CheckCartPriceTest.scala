3f34958eefae3ab949b2f6e741983c3f4b683774
Łukasz Indykiewicz <lukasz.indykiewicz@gmail.com>
2016-06-14 11:03:17 +0200

    Add 10 percent promotion test

--- src/test/scala/lettershop/CheckCartPriceTest.scala
+++ src/test/scala/lettershop/CheckCartPriceTest.scala
@@ -12,11 +12,12 @@ class CheckCartPriceTest(implicit ee: ExecutionEnv)
     extends Specification
     with BaseTckTest {
 
-  def checkCartPrice(letters: String) = {
+  def checkCartPrice(letters: String, promoCode: Option[String] = None) = {
     val cartId = UUID.randomUUID.toString
+    val promo = promoCode.map(p => s"?promo=$p").getOrElse("")
     for {
       _ <- go(HttpRequest(PUT, url(s"cart/$cartId/$letters")))
-      check <- go(HttpRequest(uri = url(s"check/$cartId")))
+      check <- go(HttpRequest(uri = url(s"check/$cartId$promo")))
     } yield check
   }
 
@@ -27,29 +28,48 @@ class CheckCartPriceTest(implicit ee: ExecutionEnv)
       price.map(_.status) should ===(ok200).awaitFor(timeout)
     }
 
-    "Return price as a sum of letters when no promotions are available" >> {
+    "return price as a sum of letters when no promotions are available" >> {
       val price = checkCartPrice("abcd")
       body(price).map(read[Price]) should ===(Price(40)).awaitFor(timeout)
     }
 
-    "Three for two for 'a' promotion with 3 'a' letters" >> {
+    "use Three For Two for 'a' promotion when 3 'a' letters" >> {
       val price = checkCartPrice("abacda")
       body(price).map(read[Price]) should ===(Price(50)).awaitFor(timeout)
     }
 
-    "Three for two for 'a' promotion with 7 'a' letters" >> {
+    "use Three For Two for 'a' promotion when 7 'a' letters" >> {
       val price = checkCartPrice("abacadaeafaga")
       body(price).map(read[Price]) should ===(Price(110)).awaitFor(timeout)
     }
 
-    "Three for two for 'a' and 'X' promotion with 4 'a' and 3 'X' letters" >> {
+    "use Three For Two for 'a' and 'X' promotion when 4 'a' and 3 'X' letters" >> {
       val price = checkCartPrice("aaaaXXX")
       body(price).map(read[Price]) should ===(Price(50)).awaitFor(timeout)
     }
 
-    "Three for two for 'a' and 'X' promotion with 2 'a' and 1 'X' letter" >> {
+    "use Three For Two for 'a' and 'X' promotion when 2 'a' and 1 'X' letter" >> {
       val price = checkCartPrice("aaX")
       body(price).map(read[Price]) should ===(Price(30)).awaitFor(timeout)
+    }
+
+  }
+
+  "CheckCartPriceTest with promo code" should {
+
+    "use '10percent' promo code for 10 percent sale" >> {
+      val price = checkCartPrice("qwerty", Some("10percent"))
+      body(price).map(read[Price]) should ===(Price(54)).awaitFor(timeout)
+    }
+
+    "other than '10percent' return regular price" >> {
+      val price = checkCartPrice("qwerty", Some("20percent"))
+      body(price).map(read[Price]) should ===(Price(60)).awaitFor(timeout)
+    }
+
+    "use all available promotions" >> {
+      val price = checkCartPrice("qaweartay", Some("10percent"))
+      body(price).map(read[Price]) should ===(Price(72)).awaitFor(timeout)
     }
 
   }
